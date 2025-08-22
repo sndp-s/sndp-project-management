497ed537a94461da90a40bf53c77bab4b0005733
Sandeep Sharma <sandeeptech8@gmail.com>
2025-08-22 08:29:06 +0530

    feat: custom user and organisation admin setup

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Organization
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "organization",
+                    "is_staff", "is_active")
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("organization",)}),
+    )
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "contact_email")

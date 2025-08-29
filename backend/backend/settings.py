dee009c5e1f14d98abd12b07928b7ab693be5adc
Sandeep Sharma <sandeeptech8@gmail.com>
2025-08-29 19:14:38 +0530

    feat: configure settings to serve static assets in prod using whitenosie

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,7 +142,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# Where collectstatic will place files (served by WhiteNoise in Docker)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Enable hashed/compressed static files for production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
